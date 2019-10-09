3312e3853c00ad5238664a7662b2f0474a1911e6
aisecret <ai_secret@yahoo.com>
2019-10-09 15:56:53 +0200

    created a random function

--- camvas-master Combination/javascripts/script.js
+++ camvas-master Combination/javascripts/script.js
@@ -2,10 +2,16 @@ const video = document.getElementById('video');
 
 var baguette = new Image();
 var croissant = new Image();
+var randomPositionX = new Number();
+var randomPositionY = new Number();
+var videoWidth = new Number();
+var videoHeight = new Number();
+
+videoWidth = video.width;
+videoHeight = video.height;
 
 baguette.src = "images/baguette.png";
 croissant.src = "images/croissant.png";
-
 
 // loading all the models async
 Promise.all([
@@ -22,22 +28,39 @@ function playVideo() {
     )
 }
 
+function getRandomNumberX() {
+    randomPositionX = Math.floor(Math.random() * (videoWidth-50));
+    return randomPositionX;
+}
+
+function getRandomNumberY() {
+    randomPositionY = Math.floor(Math.random() * videoHeight-50);
+    return randomPositionY;
+}
+
+for (i = 0; i < 5; i++) {
+    randomPositionX = getRandomNumberX();
+    randomPositionY = getRandomNumberY();
+    console.log(randomPositionX + " "+ randomPositionY);
+}    
+
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
     document.body.append(canvas);
     const displaySize = {
         width: video.width, height: video.height
     }
+
     faceapi.matchDimensions(canvas, displaySize);
     setInterval(
         async () => {
             const detections = await faceapi.detectAllFaces(video,
-                new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
             const resizedDetections = faceapi.resizeResults(detections, displaySize);
-            console.log(detections[0].landmarks.getMouth());
-            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-            canvas.getContext('2d').drawImage(baguette,0,0);
-            canvas.getContext('2d').drawImage(croissant,500,300);
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);   
+            canvas.getContext('2d').drawImage(croissant,randomPositionX,randomPositionY);
+            canvas.getContext('2d').drawImage(baguette,0,0);             
+
             faceapi.draw.drawDetections(canvas, resizedDetections);
             faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         }, 100)
